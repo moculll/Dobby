be4cbd4d6d1c513b611466169c1b04155df208e9
mocul <823309460@qq.com>
2024-10-11 18:25:10 +0800

    custom modify

---
+++ examples/build.sh
@@ -0,0 +1,13 @@
+export NDK_PATH=/home/mocul/c_test/android/android-ndk-r25c
+export TOOLCHAIN=$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64
+export TARGET=aarch64-linux-android
+export API=21
+
+# 设置编译器和工具路径
+export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
+export AR=$TOOLCHAIN/bin/$TARGET-ar
+export LD=$TOOLCHAIN/bin/$TARGET-ld
+
+$CXX -fPIC -shared -static-libstdc++ -o test.so main.cc /home/mocul/c_test/android/Dobby/build/android/arm64-v8a/libdobby.a -llog -landroid
+#$CXX -o test main.cc /home/mocul/c_test/android/Dobby/build/android/arm64-v8a/libdobby.a -llog -landroid
--- examples/main.cc
+++ examples/main.cc
@@ -4,11 +4,16 @@
 #include <string.h>
 #include <pthread.h>
 #include <iostream>
-
-int main(int argc, char const *argv[]) {
-
-  std::cout << "Start..." << std::endl;
-
-  sleep(100);
-  return 0;
+#include <android/log.h>
+#define LOG_TAG "TEST"
+void testfunc() {
+  for (int i = 0; i < 99999; i++) {
+    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "---------------------%d------------------------", i);
+    usleep(100000);
+  }
+  return;
+}
+__attribute__((constructor)) void entrypoint() {
+  pthread_t socket_server;
+  pthread_create(&socket_server, NULL, (void *(*)(void *))testfunc, NULL);
 }
